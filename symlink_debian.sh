49ca7a305109dfc15a75b1bb6ebbd6616781244b
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-03-30 22:52:43 +1100

    Added rofi.

--- symlink_debian.sh
+++ symlink_debian.sh
@@ -7,7 +7,7 @@ usrConfig=$HOME/.config
 homeConfig=$PWD/.home
 thisConfig=$PWD/.config
 
-for i in alacritty bspwm sxhkd polybar nvim i3 picom; do
+for i in alacritty bspwm sxhkd polybar nvim i3 picom rofi; do
     rm -rf $usrConfig/$i
     mkdir -p $usrConfig/$i
     ln -s $thisConfig/$i/* $usrConfig/$i/
