f953cf8f8a1193209b41b8f454797b00ce36f7b2
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-03 20:58:26 +1000

    Added gnome-keyring and libsecret for vscode.

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S --noconfirm i3-gaps alacritty xorg xorg-xset xorg-xrandr # base
-sudo pacman -S --noconfirm firefox nautilus nitrogen rofi # base
+sudo pacman -S --noconfirm firefox nautilus nitrogen rofi gnome-keyring libsecret # base
 
 ./symlink.sh
