a28b5ed744a75c0bb572454689a0c056d508521f
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-03 21:47:25 +1000

    Added dracula theme to alacritty.

--- arch.sh
+++ arch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm i3-gaps alacritty xorg xorg-xset xorg-xrandr # base
-sudo pacman -S --noconfirm firefox nautilus nitrogen rofi gnome-keyring libsecret # base
+sudo pacman -S --noconfirm i3-gaps alacritty xorg xorg-xset xorg-xrandr git # base
+sudo pacman -S --noconfirm firefox nautilus nitrogen rofi gnome-keyring libsecret lxappearance lxsession # base
+
+pacman -S --needed --noconfirm base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 ./symlink.sh
+
