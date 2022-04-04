87c821e42592f5b30188bc40c21fd885a0f96690
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-04 11:18:42 +1000

    added redshift

--- arch.sh
+++ arch.sh
@@ -7,7 +7,7 @@ makepkg -si
 cd ..
 rm -rf yay
 
-sudo pacman -S --noconfirm i3-gaps alacritty xorg xorg-xset xorg-xrandr git
+sudo pacman -S --noconfirm i3-gaps alacritty xorg xorg-xset xorg-xrandr git redshift
 sudo pacman -S --noconfirm firefox nautilus nitrogen rofi gnome-keyring
 sudo pacman -S --noconfirm libsecret lxappearance lxsession dunst github-cli
 
