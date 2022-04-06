561f7c5dd344e09754a99e9c9963ba8d983f8cb7
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-06 23:11:46 +1000

    added redshift, dpi scaling, better synclient drivers, adjusted alacritty and picom. phew

--- symlink.sh
+++ symlink.sh
@@ -7,16 +7,25 @@ usrConfig=$HOME/.config
 homeConfig=$PWD/.home
 thisConfig=$PWD/.config
 
+# Root .config dotfiles
+for i in redshift.conf; do
+    rm -rf $usrConfig/$i
+    ln -s $thisConfig/$i $usrConfig/$i
+done
+
+# .config sub dir dotfiles
 for i in alacritty polybar nvim i3 picom rofi dunst; do
     rm -rf $usrConfig/$i
     mkdir -p $usrConfig/$i
     ln -s $thisConfig/$i/* $usrConfig/$i/
 done
 
-for i in .vimrc .gitconfig .bashrc .Xresources; do
+# .home dotfiles
+for i in .vimrc .gitconfig .bashrc .Xresources .xinitrc; do
     rm -rf $HOME/$i
     ln -s $homeConfig/$i $HOME/$i
 done
 
+# /etc/ stuff
 sudo cp -f $HOME/.dotfiles/etc/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
 
