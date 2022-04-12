291bd9732438ab3f60e4336d0c154165492f7086
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-12 21:46:38 +1000

    changes.

--- scripts/displays.sh
+++ scripts/displays.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 int=eDP-1 # Internal screen
-ext1=DVI-I-2-2 # Main 16:9 screen
-ext2=DVI-I-1-1 # Secondary 16:10 screen
+ext1=DVI-I-1-1 # Main 16:9 screen
+ext2=DVI-I-2-2 # Secondary 16:10 screen
 ext3=HDMI-2 # HDMI output
 
+nr=nitrogen --restore
+
 case "$1" in
-	"--undocked") xrandr --output "$int" --primary --auto --output "$ext1" --off --output "$ext2" --off --output "$ext3" --off ;;
-	"--homedock") xrandr --output "$int" --off --output "$ext1" --primary --auto --output "$ext2" --left-of "$ext1" --auto --output "$ext3" --off ;;
-	"--workdock") xrandr --output "$int" --left-of "$ext3" --auto --output "$ext1" --off --output "$ext2" --off --output "$ext3" --primary --auto ;;
+	"--undocked") $nr & xrandr --output "$int" --primary $dpi2 --auto --output "$ext1" --off --output "$ext2" --off --output "$ext3" --off ;;
+	"--homedock") $nr & xrandr --output "$int" --off --output "$ext1" --primary $dpi1 --auto --output "$ext2" --left-of "$ext1" $dpi1 --auto --output "$ext3" --off ;;
+	"--workdock") $nr & xrandr --output "$int" --left-of "$ext3" --auto --output "$ext1" --off --output "$ext2" --off --output "$ext3" --primary --auto ;;
 	*) exit 1 ;;
 esac
 
