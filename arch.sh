450e92ec8445bad19cf9524a779596790ba2f7cb
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-13 08:21:51 +1000

    Added path to inst script.

--- arch.sh
+++ arch.sh
@@ -9,9 +9,16 @@ rm -rf yay
 
 yay -S --needed --noconfirm - < scripts/packages.txt
 
+sudo systemctl enable gdm.service
+sudo systemctl enable bluetooth.service
+sudo systemctl enable cronie.service
+
 nitrogen --set-scaled wallpapers/pixel_dark_village.jpeg
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ./symlink.sh
+
+export PATH=~/.local/bin/:$PATH
+
