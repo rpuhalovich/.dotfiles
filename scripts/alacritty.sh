9981f6257b6f30b6546364867435b66acee9355e
Ryan Puhalovich <reanpuhalovich@gmail.com>
2022-04-29 06:05:22 +1000

    Added a bunch of stuff and updated.

--- scripts/alacritty.sh
+++
@@ -1,5 +0,0 @@
-# Install Alacritty
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-cargo install alacritty
-sudo mv ~/.cargo/bin/alacritty /usr/local/bin
---
+++ scripts/output/undocked.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-2-2 --off --output DVI-I-1-1 --off
+
+$HOME/.dotfiles/scripts/output/_post.sh
