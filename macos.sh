f8b74047d0f71e2ceedd73e600597515cf0a501f
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-03-14 01:01:51 +1100

    Moved alacritty to .home

--- macos.sh
+++ macos.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-for i in .clangd .hammerspoon .gitconfig .vimrc .zshrc .tmux.conf; do
+for i in .clangd .hammerspoon .gitconfig .vimrc .zshrc .tmux.conf .alacritty.yml; do
     rm -rf $HOME/$i
     ln -s $HOME/.dotfiles/.home/$i $HOME/$i
 done
 
 mkdir -p $HOME/.config/
-for i in nvim btop alacritty; do
+for i in nvim btop; do
     rm -rf $HOME/.config/$i
     ln -s $HOME/.dotfiles/.home/.config/$i $HOME/.config/$i
 done
