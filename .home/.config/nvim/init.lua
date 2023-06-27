76e6e967fc147f7bbcbdcaba98aff3a7d5a4c523
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-06-27 11:27:52 +1000

    changes.

--- .home/.config/nvim/init.lua
+++ .home/.config/nvim/init.lua
@@ -4,6 +4,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "k", [[gk]])
 vim.keymap.set("n", "j", [[gj]])
+vim.keymap.set("n", "A", [[g$a]])
+vim.keymap.set("n", "$", [[g$]])
+vim.keymap.set("n", "0", [[g0]])
 
 vim.keymap.set("n", "<leader>a", ":%y+<cr>")
 
