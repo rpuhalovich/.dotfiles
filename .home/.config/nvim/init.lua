331f2a6c2a0d777d3249c8702b11abb4095a1b55
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-07-19 12:50:26 +1000

    Now added wildignore to default find.

--- .home/.config/nvim/init.lua
+++ .home/.config/nvim/init.lua
@@ -81,8 +81,8 @@ vim.o.background = "dark"
 vim.opt.autochdir = false
 vim.opt.autoread = true
 vim.opt.backup = false
-vim.opt.cursorline = true
 vim.opt.colorcolumn = ""
+vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.guicursor = ""
 vim.opt.hlsearch = true
@@ -109,6 +109,7 @@ vim.opt.termguicolors = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.updatetime = 50
+vim.opt.wildignore:append("**/node_modules/**")
 vim.opt.wrap = true
 
 -- show cursor line only in active window
