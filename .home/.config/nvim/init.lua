8e871737aef3622b3e55bd0adc221b64524b1b23
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-08-01 22:07:07 +1000

    changes

--- .home/.config/nvim/init.lua
+++ .home/.config/nvim/init.lua
@@ -153,12 +153,12 @@ vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 
 -- auto update the highlight style on colorscheme change
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  pattern = { "*" },
-  callback = function(ev)
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
-  end
+    pattern = { "*" },
+    callback = function(ev)
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+    end
 })
 
 -- LSP --
