acd6129505e774fff32a8e705f6301eb1962db18
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-08-31 10:12:18 +1000

    removed commented out stuff

--- .home/.config/nvim/init.lua
+++ .home/.config/nvim/init.lua
@@ -12,6 +12,9 @@ vim.keymap.set("v", "A", [[g$a]])
 vim.keymap.set("v", "j", [[gj]])
 vim.keymap.set("v", "k", [[gk]])
 
+-- K is annoying
+vim.keymap.set("n", "K", [[]])
+
 -- Copy Whole File
 vim.keymap.set("n", "<leader>a", [[:%y+<cr>]])
 
@@ -33,8 +36,6 @@ vim.keymap.set("n", "<leader>s", [[<C-w><C-s><C-w>j]])
 
 -- Auto Pairs
 vim.keymap.set("i", "{<cr>", [[{<cr>}<Esc>O]])
--- vim.keymap.set("i", "(<cr>", [[(<cr>)<Esc>O]])
--- vim.keymap.set("i", "[<cr>", [[[<cr>]<Esc>O]])
 
 -- Open Explorer
 vim.keymap.set("n", "<leader>e", [[:e .<cr>]])
@@ -52,9 +53,7 @@ vim.keymap.set("n", "N", [[Nzzzv]])
 -- Copying and pasting
 vim.keymap.set("n", "<leader>p", [["+p]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
--- vim.keymap.set({"n", "v"}, "<leader>yy", [[mm_v$h"+y`m]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["+d]])
--- vim.keymap.set({"n", "v"}, "<leader>dd", [[mm_v$h"+y`mdd]])
 
 -- Close all buffers except open one
 vim.keymap.set("n", "<leader>B", [[:%bd|e#<cr>]])
@@ -117,7 +116,6 @@ vim.opt.shiftwidth = 4
 vim.opt.signcolumn = "yes"
 vim.opt.smartindent = true
 vim.opt.softtabstop = 4
--- vim.opt.statusline = "%<%F %h%m%r%=%-16.(%l,%c%V%) %P"
 vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
@@ -127,15 +125,6 @@ vim.opt.undofile = true
 vim.opt.updatetime = 50
 vim.opt.wildignore:append("**/node_modules/**")
 vim.opt.wrap = true
-
--- relative number only in the active window and only in normal mode
--- vim.cmd([[
---     augroup numbertoggle
---         autocmd!
---         autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu | endif
---         autocmd BufLeave,FocusLost,InsertEnter,WinLeave * if &nu | set nornu | endif
---     augroup END
--- ]])
 
 -- no auto comments anywhere
 vim.cmd([[autocmd FileType * set formatoptions-=cro]])
