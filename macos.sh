2110ca20e46d9a2d42fcd372d29d1c20a6be7dde
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-01-26 12:11:08 +1100

    Move alacritty config

--- macos.sh
+++ macos.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
-for i in .ideavimrc .clangd .scripts .hammerspoon .gitconfig .vimrc .zshrc .tmux.conf .alacritty.toml; do
+for i in .ideavimrc .clangd .scripts .hammerspoon .gitconfig .vimrc .zshrc .tmux.conf; do
     rm -rf $HOME/$i
     ln -s $HOME/.dotfiles/.home/$i $HOME/$i
 done
 mkdir -p $HOME/.scripts-local/
 
 mkdir -p $HOME/.config/
-for i in nvim btop; do
+for i in nvim btop alacritty; do
     rm -rf $HOME/.config/$i
     ln -s $HOME/.dotfiles/.home/.config/$i $HOME/.config/$i
 done
