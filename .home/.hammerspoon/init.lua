269d74570df26958e491d0f401fff9dccf2d7cc6
Ryan Puhalovich <reanpuhalovich@gmail.com>
2025-09-08 21:09:02 +1000

    Update hammerspoon

--- .home/.hammerspoon/init.lua
+++ .home/.hammerspoon/init.lua
@@ -1,7 +1,7 @@
 -- maximize on new window
 local wfTerminal = hs.window.filter.new()
 wfTerminal:subscribe(hs.window.filter.windowCreated, function(window, applicationName)
-  for _,a in pairs({"Preview", "Alacritty", "Finder", "Code", "VSCodium"}) do
+  for _,a in pairs({"Finder", "Preview", "Alacritty"}) do
     if a == applicationName then window:maximize() end
   end
 end)
@@ -10,7 +10,6 @@ end)
 hs.hotkey.bind({"cmd", "shift"}, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
 -- app shortcuts
-hs.hotkey.bind({"cmd"}, "1", function() hs.application.launchOrFocus("Firefox") end)
-hs.hotkey.bind({"cmd"}, "2", function() hs.application.launchOrFocus("Firefox Developer Edition") end)
+hs.hotkey.bind({"cmd"}, "2", function() hs.application.launchOrFocus("LibreWolf") end)
 hs.hotkey.bind({"cmd"}, "3", function() hs.application.launchOrFocus("Alacritty") end)
 hs.hotkey.bind({"cmd"}, "4", function() hs.application.launchOrFocus("Xcode") end)
